c544bff03fd3188261edd2f1de8b7cd0869e1ab5
Vyacheslav Bek <slavabek.games@gmail.com>
2023-11-30 18:07:29 +0300

    Hi

---
+++ obuchaika/main.cpp
@@ -0,0 +1,19 @@
+﻿#include <clocale>
+#include <iostream>
+
+#include "menu.hpp"
+#include "menu_functions.hpp"
+#include "menu_items.hpp"
+
+int main() {
+	std::setlocale(LC_ALL, "");
+
+	std::cout << "Обучайка приветствует тебя!" << std::endl;
+
+	const VBek::MenuItem* current = &VBek::MAIN;
+	do {
+		current = current->func(current);
+	} while (true);
+
+	return 0;
+}
